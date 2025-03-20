a0bbcc4e58b60f71f7f41184ad1d265153c9538b
Udav-94 <ups-fti@yandex.ru>
2025-03-20 19:55:43 +0300

    Замена работы с useRef на useState

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import uniqid from 'uniqid';
 import './App.css'
 import { validateCity } from './functionsForApp.js'
@@ -8,25 +8,28 @@ let elem = [];
 
 function App() {
 
-  const cityName = useRef(null);
-  const timtOffset = useRef(null);
+
+  const [cityName, setCityName] = useState('');
+  const [timtOffset, setTimtOffset] = useState('');
   const [newElem, setElem] = useState(0);
+
+  
   
 
   function onButtonClick(e) {
     e.preventDefault();
-    let city = cityName.current.value;
+    let city = cityName;
 
     if (validateCity (city) ){
-      /* console.log(city); */
-      cityName.current.value='';
+      setCityName('');
+      
     } else {
-      cityName.current.value='';
+      setCityName('');
       alert('Не верный формат ввода');
       return
     }
-    elem.push(<Clock key={uniqid()} cityName={city}  Offset={timtOffset.current.value == '' ? "0" : timtOffset.current.value } onClick={ e => {closeClock(e)}}/>);
-    timtOffset.current.value='';
+    elem.push(<Clock key={uniqid()} cityName={city}  Offset={timtOffset == '' ? "0" : timtOffset } onClick={ e => {closeClock(e)}}/>);
+    setTimtOffset('');
     setElem(newElem + 1);
   }
 
@@ -46,12 +49,12 @@ function App() {
         <div className="app-box__form__elem">
           <label htmlFor="cityName" className="app-box__form__elem__label" >Название</label>
           <br/>
-          <input className="app-box__form__elem__input" type="text" id ="cityName" ref={ cityName }/>
+          <input className="app-box__form__elem__input" type="text" id ="cityName" value={cityName} onChange={(e) => {setCityName(e.target.value)}} /* ref={ cityName } *//>
         </div>
         <div className="app-box__form__elem">
           <label htmlFor="way" className="app-box__form__elem__label">Временная зона</label>
           <br/>
-          <input className="app-box__form__elem__input" type="text" id ="way" ref={ timtOffset }/>
+          <input className="app-box__form__elem__input" type="number" min="-12" max="12" id ="way" value={timtOffset} onChange={(e) => {setTimtOffset(e.target.value)}} /* ref={ timtOffset } *//>
         </div>
         <button className="app-box__form__but" onClick={onButtonClick} >Добавеить</button>
       </form>
